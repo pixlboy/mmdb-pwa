0538125340bf58d0378f9304784bade443a37e66
Rachit Garg <rachigarg@gmail.com>
2020-10-24 19:59:14 +0530

    env variables added

--- src/Db.js
+++ src/Db.js
@@ -16,30 +16,3 @@ Db.enablePersistence()
 });
 
 export default Db;
-
-// // add new recipe
-// // const form = document.querySelector('form');
-// // form.addEventListener('submit', evt => {
-// //   evt.preventDefault();
-  
-// //   const recipe = {
-// //     name: form.title.value,
-// //     ingredients: form.ingredients.value
-// //   };
-
-// //   db.collection('recipes').add(recipe)
-// //     .catch(err => console.log(err));
-
-// //   form.title.value = '';
-// //   form.ingredients.value = '';
-// // });
-
-// // // remove a recipe
-// // const recipeContainer = document.querySelector('.recipes');
-// // recipeContainer.addEventListener('click', evt => {
-// //   if(evt.target.tagName === 'I'){
-// //     const id = evt.target.getAttribute('data-id');
-// //     //console.log(id);
-// //     db.collection('recipes').doc(id).delete();
-// //   }
-// // })
